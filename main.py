f4fc1954c5a038ec2bc56cb82f6361e936e00dc2
taiteng <taitengchan@gmail.com>
2023-10-25 22:08:07 +0800

    model

---
+++ main.py
@@ -0,0 +1,175 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import datetime as dt
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import requests
+
+
+app = Flask(__name__)
+
+
+CORS(app)
+
+
+def get_data():
+    url = 'https://min-api.cryptocompare.com/data/histoday'
+    params = {'fsym': 'BTC', 'tsym': 'USD', 'limit': 2000}
+
+    response = requests.get(url, params=params)
+    data = response.json()['Data']
+    df = pd.DataFrame(data)
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    df.set_index('time', inplace=True)
+
+    start = dt.datetime(2011, 1, 1)
+    end = dt.datetime.now()
+    delta = end - start
+
+    while delta.days > 0:
+        if delta.days >= 2000:
+            date = end - dt.timedelta(days=2000)
+            params = {'fsym': 'BTC', 'tsym': 'USD', 'limit': 2000,
+                      'toTs': date.timestamp()}
+        else:
+            params = {'fsym': 'BTC', 'tsym': 'USD', 'limit': delta.days,
+                      'toTs': df.index[0].timestamp()}
+        response = requests.get(url, params=params)
+        data = response.json()['Data']
+        df1 = pd.DataFrame(data)
+        df1['time'] = pd.to_datetime(df1['time'], unit='s')
+        df1.set_index('time', inplace=True)
+        delta -= dt.timedelta(days=2000)
+        df = pd.concat([df1, df], axis=0)
+    btcData = pd.DataFrame(df)
+    btcData = btcData.dropna()
+    cols = ["conversionType", "conversionSymbol", "volumefrom"]
+    btcData = btcData.drop(columns=cols, axis=1)
+
+    return btcData
+
+
+def build_lstm_model(btcData):
+    scaler = MinMaxScaler()
+    data_sc = scaler.fit_transform(btcData['close'].values.reshape(-1, 1))
+
+    prediction_days = 60
+    future_days = 30
+    x_train, y_train = [], []
+
+    for x in range(prediction_days, len(data_sc) - future_days):
+        x_train.append(data_sc[x - prediction_days:x, 0])
+        y_train.append(data_sc[x + future_days, 0])
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    model = Sequential()
+
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=30))
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+    return model
+
+
+def test_predict(model, btcData):
+    scaler = MinMaxScaler()
+    prediction_days = 60
+    test_start = '2020-01-01'
+    test_data = btcData.iloc[btcData.index >= test_start].copy()
+
+    total_data = pd.concat([btcData['close'], test_data['close']])
+
+    model_inputs = total_data[len(total_data) - len(test_data) - prediction_days:].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.fit_transform(model_inputs)
+
+    x_test = []
+
+    for x in range(prediction_days, len(model_inputs)):
+        x_test.append(model_inputs[x - prediction_days:x, 0])
+    x_test = np.array(x_test, dtype=np.float32)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    prediction_price = model.predict(x_test)
+    prediction_price = scaler.inverse_transform(prediction_price)
+
+    return prediction_price
+
+
+def make_predictions(model, btcData):
+    scaler = MinMaxScaler()
+    prediction_days = 60
+    future_days = 30
+
+    test_start = '2020-01-01'
+    test_data = btcData.iloc[btcData.index >= test_start].copy()
+
+    total_data = pd.concat([btcData['close'], test_data['close']])
+
+    model_inputs = total_data[len(total_data) - len(test_data) - prediction_days:].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.fit_transform(model_inputs)
+
+    real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs) + 1, 0]]
+    real_data = np.array(real_data)
+    real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
+
+    prediction = model.predict(real_data)
+    prediction = scaler.inverse_transform(prediction)
+
+    prediction = np.reshape(prediction, future_days)
+
+    return prediction
+
+
+def calculate_accuracy(btcData, test_predictions):
+    test_start = '2020-01-01'
+    test_data = btcData.iloc[btcData.index >= test_start].copy()
+    actual_price = test_data['close'].values
+
+    if len(actual_price) != len(test_predictions):
+        raise ValueError("Lengths of actual and predicted values must be the same.")
+
+    absolute_errors = [abs(actual - predicted) for actual, predicted in zip(actual_price, test_predictions)]
+    mean_absolute_error = sum(absolute_errors) / len(absolute_errors)
+    mean_actual_value = sum(actual_price) / len(actual_price)
+    percentage_accuracy = (1 - mean_absolute_error / mean_actual_value) * 100
+    accuracy_values = np.array(percentage_accuracy)
+    mean_accuracy = np.mean(accuracy_values)
+
+    return mean_accuracy
+
+
+@app.route('/')
+def predict():
+    try:
+        btcData = get_data()
+
+        model = build_lstm_model(btcData)
+
+        test_predictions = test_predict(model, btcData)
+
+        mean_absolute_error_percentage = calculate_accuracy(btcData, test_predictions)
+
+        predictions = make_predictions(model, btcData)
+
+        prediction = predictions.tolist()
+
+        return jsonify({'Prediction': str(prediction), 'Accuracy': str(f"{mean_absolute_error_percentage:.2f}%")})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
